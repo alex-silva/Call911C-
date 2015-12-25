902cacf4b2663a3ea9b8ce3f5fa9b161568d94a8
alex-silva <abriskiev@ltu.edu>
2015-12-25 20:19:43 -0200

    New classes

---
+++ Helpers/Config.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace Helpers
+{
+    public static class Config
+    {
+        private static readonly object _lock = new object();
+
+        public static bool ReadBoolAppSettings(string field, bool Default)
+        {
+            return Boolean.Parse(LeAppSettings(field, Default.ToString()));
+        }
+
+        public static void WriteBoolAppSettings(string field, bool Valor)
+        {
+            GravaAppSettings(field, Valor.ToString());
+        }
+
+        public static string ReadSettings()
+        {
+            lock (_lock)
+            {
+                StringBuilder ret = new StringBuilder();
+                Configuration config = null;
+                try
+                {
+                    config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                }
+                catch
+                {
+                    config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoaming);
+                }
+
+                foreach (var item in config.AppSettings.Settings.AllKeys)
+                {
+                    ret.AppendLine(item.ToString() + " = " + config.AppSettings.Settings[item].Value.Trim());
+                }
+                return ret.ToString();
+            }
+        }
+
+        /// <summary>
+        /// Read an item from current AppSettings
+        /// </summary>
+        /// <param name="field">Field to read</param>
+        /// <param name="defaultValue">Default Value, if exists</param>
+        /// <returns>Field Value</returns>
+        public static string LeAppSettings(string field, string defaultValue)
+        {
+            string valor = defaultValue.Trim();
+            lock (_lock)
+            {
+                Configuration config = null;
+                try
+                {
+                    config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                }
+                catch
+                {
+                    config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoaming);
+                }
+                if (config.AppSettings.Settings.AllKeys.Contains(field))
+                    valor = config.AppSettings.Settings[field].Value.Trim();
+                else
+                {
+                    config.AppSettings.Settings.Add(field, defaultValue);
+                    config.Save();
+                }
+                if (string.IsNullOrEmpty(valor) && !string.IsNullOrEmpty(defaultValue))
+                {
+                    valor = defaultValue.Trim();
+                    config.AppSettings.Settings[field].Value = valor;
+                    config.Save();
+                }
+            }
+            return valor;
+        }
+
+        /// <summary>
+        /// Write data from current AppSettings
+        /// </summary>
+        /// <param name="field">Field to write </param>
+        /// <param name="value">Field value</param>
+        public static void GravaAppSettings(string field, string value)
+        {
+            lock (_lock)
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                if (config.AppSettings.Settings.AllKeys.Contains(field))
+                    config.AppSettings.Settings[field].Value = value.Trim();
+                else
+                    config.AppSettings.Settings.Add(field, value);
+                config.Save();
+                try
+                {
+                    ConfigurationManager.RefreshSection("AppSettings");
+                }
+                catch { }
+            }
+        }
+
+        public static ConnectionStringSettings ReadConnSetting(string Name)
+        {
+            return ReadConnSetting(Name, null);
+        }
+
+        public static ConnectionStringSettings ReadConnSetting(string Name, string Conn, string Provider)
+        {
+            return ReadConnSetting(Name, new ConnectionStringSettings(Name, Conn, Provider));
+        }
+
+
+        public static ConnectionStringSettings ReadConnSetting(string Name, ConnectionStringSettings Conn)
+        {
+            ConnectionStringSettings Ret = null;
+
+            lock (_lock)
+            {
+                try
+                {
+                    Ret = ConfigurationManager.ConnectionStrings[Name];
+                }
+                catch { }
+
+                if (Ret == null && Conn != null)
+                {
+                    try
+                    {
+                        ConfigurationManager.ConnectionStrings.Add(Conn);
+                    }
+                    catch { }
+                    Ret = Conn;
+                }
+            }
+
+
+            return Ret;
+        }
+
+        public static void WriteConnSetting(ConnectionStringSettings Conn)
+        {
+            ConnectionStringSettings Ret = null;
+
+            lock (_lock)
+            {
+                Configuration config = ConfigurationManager.OpenExeConfiguration(Application.ExecutablePath);
+                try
+                {
+                    Ret = config.ConnectionStrings.ConnectionStrings[Conn.Name];
+                }
+                catch { }
+
+                if (Ret == null)
+                {
+                    config.ConnectionStrings.ConnectionStrings.Add(Conn);
+                }
+                else
+                {
+                    Ret.ProviderName = Conn.ProviderName;
+                    Ret.ConnectionString = Conn.ConnectionString;
+                }
+
+                config.Save();
+                try
+                {
+                    ConfigurationManager.RefreshSection("connectionStrings");
+                }
+                catch { }
+            }
+        }
+    }
+}
---
+++ Helpers/CustomFile.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Helpers
+{
+    public static class CustomFile
+    {
+        public static bool IsFileLocked(string filePath)
+        {
+            try
+            {
+                using (File.Open(filePath, FileMode.Open)) { }
+            }
+            catch (IOException e)
+            {
+                var errorCode = Marshal.GetHRForException(e) & ((1 << 16) - 1);
+
+                return errorCode == 32 || errorCode == 33;
+            }
+
+            return false;
+        }
+
+        public static string ComputeHash(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                return Convert.ToBase64String(md5.ComputeHash(File.ReadAllBytes(filePath)));
+            }
+        }
+
+        /// <summary>
+        /// Verifies if the file exists, if so, adds the string "Copy" to the end of the file.
+        /// </summary>
+        /// <param name="pStrFilePath"></param>
+        /// <returns></returns>
+        public static string renameFileName(string pStrFilePath)
+        {
+            while (File.Exists(pStrFilePath))
+            {
+                string path = Path.GetDirectoryName(pStrFilePath);
+                string fileName = Path.GetFileNameWithoutExtension(pStrFilePath);
+                string fileExtension = Path.GetExtension(pStrFilePath);
+                pStrFilePath = Path.Combine(path, fileName + " - Copy" + fileExtension);
+            }
+            return pStrFilePath;
+        }
+
+        /// <summary>
+        /// Gets a byte[] from the complete path of a file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public static byte[] getBytesFromFile(string filePath)
+        {
+            byte[] file;
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = new BinaryReader(stream))
+                {
+                    file = reader.ReadBytes((int)stream.Length);
+                }
+            }
+            return file;
+        }
+
+        /// <summary>
+        /// Saves a file in the destination dir/file from a byte[]
+        /// </summary>
+        /// <param name="pObjBytes"></param>
+        /// <param name="destinationFilePath"></param>
+        public static void getFileFromBytes(byte[] pObjBytes, string destinationFilePath)
+        {
+            var blob = pObjBytes.ToArray();
+            using (var fs = new FileStream(destinationFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                fs.Write(blob, 0, blob.Length);
+        }
+    }
+}
